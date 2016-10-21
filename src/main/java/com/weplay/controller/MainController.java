68c2897515f62c825eb9d8ef3bddfdcfd7d746b2
wenbo <snakefwb2@gmail.com>
2016-10-21 17:02:11 +0800

    UPdate

--- src/main/java/com/weplay/controller/MainController.java
+++ src/main/java/com/weplay/controller/MainController.java
@@ -1,11 +1,13 @@
 package com.weplay.controller;
 
 import com.weplay.Pojo.User;
-import com.weplay.service.IUserService;
+import com.weplay.dao.UserDao;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
 
@@ -14,13 +16,22 @@ import javax.annotation.Resource;
  */
 @Controller
 public class MainController {
-    @Resource
-    protected IUserService userService;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String index(Model model) {
-        User user = this.userService.getUserById("d4c774319ad04f38a124c0993fbc19a9");
-        model.addAttribute("user",user);
-        return "index";
+    public ModelAndView index() {
+        ApplicationContext ctx=null;
+        ctx=new ClassPathXmlApplicationContext("classpath:conf/spring-mybatis.xml");
+
+        UserDao userDao=(UserDao) ctx.getBean("userDao");
+
+        User user=null;
+        //添加两条数据
+        //user.setUserId("d4c774319ad04f38a124c0993fbc1234");
+        //userDao.insert(user);
+        user = userDao.selectByPrimaryKey("d4c774319ad04f38a124c0993fbc19a9");
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("username",user.getUsername());
+        mav.setViewName("/index");
+        return mav;
     }
 }
--- src/main/java/com/weplay/service/IUserService.java
+++ src/main/java/com/weplay/service/IUserService.java
@@ -6,5 +6,5 @@ import com.weplay.Pojo.User;
  * Created by Wenbo on 16/10/21.
  */
 public interface IUserService {
-    public User getUserById(String userId);
+    User getUserById(String userId);
 }
