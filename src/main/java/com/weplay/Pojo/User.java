55428ba744d7fb1abe79d2425876281245f9e1cd
wenbo <snakefwb2@gmail.com>
2016-10-21 16:31:40 +0800

    DB

---
+++ src/main/java/com/weplay/Pojo/User.java
@@ -0,0 +1,63 @@
+package com.weplay.Pojo;
+
+public class User {
+    private String userId;
+
+    private Boolean isadmin;
+
+    private Boolean isactive;
+
+    private String username;
+
+    private String password;
+
+    private String phoneNumber;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId == null ? null : userId.trim();
+    }
+
+    public Boolean getIsadmin() {
+        return isadmin;
+    }
+
+    public void setIsadmin(Boolean isadmin) {
+        this.isadmin = isadmin;
+    }
+
+    public Boolean getIsactive() {
+        return isactive;
+    }
+
+    public void setIsactive(Boolean isactive) {
+        this.isactive = isactive;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber == null ? null : phoneNumber.trim();
+    }
+}
---
+++ src/main/java/com/weplay/dao/UserDao.java
@@ -0,0 +1,17 @@
+package com.weplay.dao;
+
+import com.weplay.Pojo.User;
+
+public interface UserDao {
+    int deleteByPrimaryKey(String userId);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(String userId);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
